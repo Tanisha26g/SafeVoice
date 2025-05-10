3ae77c6226dfe27151ca9f62d757873a15c627a1
Tanisha26g <tanishag.2608@gmail.com>
2025-05-10 20:45:26 +0530

    Fix: Updated tsparticles import and cleaned dependencies

--- src/Components/particles.js
+++ src/Components/particles.js
@@ -1,4 +1,4 @@
-import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { Particles, initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
 // import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
 // import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
